9b22ae96d72e8ec0955e86596f9fac26581adb3d
caomengrui <fm8448512@outlook.com>
2020-11-16 23:54:23 +0800

    1. 有关http知识的提交；2. 简单实现new方法

--- Function.js
+++ Function.js
@@ -40,3 +40,28 @@ function throttle(fn, delay=1000, ...args) {
 		},delay)
 	}
 }
+
+// 3. 手写NEW
+/**
+ * new一个对象的过程
+ * 以构造器的prototype属性为原型，创建新对象
+ * 将this和参数传给构造器，执行
+ * 返回这个对象，如果默认没返回，则返回第一步创建的对象
+ * 例子
+ * 
+ * // 构造器函数
+		let Parent = function (name, age) {
+				this.name = name;
+				this.age = age;
+		};
+		Parent.prototype.sayName = function () {
+			console.log(this.name);
+		};
+ */
+function myNew(parent, ...args) {
+	if(parent.prototype) {
+		let child = Object.create(parent.prototype);
+		let result = parent.apply(child, args);
+		return typeof result === "object" ? result : child;
+	}
+}
